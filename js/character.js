2a5965f7b4f615570fc0b67127a57641167a4922
Wouter Vlaeyen <36659001+Vl-Wouter@users.noreply.github.com>
2018-09-26 21:57:23 +0200

    Finished first try of the random character generator

---
+++ js/character.js
@@ -0,0 +1,19 @@
+const ctx = document.getElementById('char').getContext('2d');
+
+function draw(x, y) {
+    ctx.beginPath();
+    ctx.rect(20 + x * 40, 20 + y * 40, 40, 40);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+}
+
+for( let x = 0; x < 12; x++) {
+    for(let y = 0; y < 13; y++) {
+        if(Math.random() < 0.5) {
+            draw(x, y);
+            if(x < 11) {
+                draw(12 - x, y);
+            }
+        }
+    }
+}
