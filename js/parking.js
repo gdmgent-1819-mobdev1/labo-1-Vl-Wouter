819a3837104d8e454242207455e728cb31331ab5
Wouter Vlaeyen <36659001+Vl-Wouter@users.noreply.github.com>
2018-09-30 16:52:34 +0200

    Updated parking
    Updated to include the icons by comparison via localStorage

--- js/parking.js
+++ js/parking.js
@@ -1,8 +1,16 @@
-let icon = 'equals';
-let oldplaces = [];
+function getSpots() {
+  return localStorage.getItem('parkingSpot').split(',');
+
+}
+
 function getData() {
   fetch('https://datatank.stad.gent/4/mobiliteit/bezettingparkingsrealtime.json').then(function(response) {
     response.json().then(function(data) {
+      let oldspots = [];
+      let newspots = [];
+      if(localStorage.getItem('parkingSpot') != null) {
+        oldspots = getSpots();
+      }
       document.body.innerHTML = '';
       for(let i = 0; i < data.length; i++) {
         let firstday = data[i].openingTimes[0].days[0];
@@ -12,6 +20,18 @@ function getData() {
         let total = data[i].parkingStatus.totalCapacity;
         let available = data[i].parkingStatus.availableCapacity;
         let col = '';
+
+        if(localStorage.getItem('parkingSpots') != null) {
+          if(oldspots[i] < data[i].parkingStatus.availableCapacity) {
+              icon = `<i class='fas fa-arrow-up'></i>`;
+          }
+          else if(oldspots[i] > data[i].parkingStatus.availableCapacity) {
+              icon = `<i class='fas fa-arrow-down'></i>`;
+          }
+          else {
+              icon = `<i class='fas fa-equals'></i>`;
+          }
+        }
 
         if((available/total) < 0.2) {
           col = 'red';  
@@ -36,12 +56,13 @@ function getData() {
           <div class='spaces'>
             <p>Totaal: <br><span class="num">${total}</span></p>
             <p>Vrij: <br><span class="num open">${available}</span></p>
-            <p><i class="icon fas fa-${icon}"></i></p>
+            <p>${icon}</p>
           </div>
         </div>  
         `;
+        newspots.push(data[i].parkingStatus.availableCapacity);
       }
-
+      localStorage.setItem('parkingSpot', newspots.toString());
       
     });
   });
